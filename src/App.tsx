d6f103966f619a7fef97eaff1d2452382a399f81
teukusem <semteuku02@gmail.com>
2024-09-04 23:36:53 +0700

    update/ test ocr camera for purpose reason

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { useRef, useState, useCallback } from "react";
+import Webcam from "react-webcam";
+import "./index.css";
+
+const videoConstraints = {
+  width: 720,
+  height: 360,
+  facingMode: "environment"
+};
+
+const App = () => {
+  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setUrl(imageSrc);
+    }
+  }, [webcamRef]);
+
+  return (
+    <>
+      <header>
+        <h1  style={{color:'black'}}>camera app</h1>
+      </header>
+      {isCaptureEnable || (
+        <button onClick={() => setCaptureEnable(true)}>start</button>
+      )}
+      {isCaptureEnable && (
+        <>
+          <div>
+            <button onClick={() => setCaptureEnable(false)}>end </button>
+          </div>
+          <div>
+            <Webcam
+              audio={false}
+              width={540}
+              height={360}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+            />
+          </div>
+          <button onClick={capture}>capture</button>
+        </>
+      )}
+      {url && (
+        <>
+          <div>
+            <button
+              onClick={() => {
+                setUrl(null);
+              }}
+            >
+              delete
+            </button>
+          </div>
+          <div>
+            <img src={url} alt="Screenshot" />
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default App
